933e8c5845fbeac1db3338391f8c181151e438a3
EdgarYeh <elecrt0620@gmail.com>
2021-10-20 00:06:33 +0800

    建立並安裝 tailwind 基礎設施

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import { React } from "react";
+
+const App=()=>{
+    return (
+        <div>
+            <h1>APP - App.js</h1>
+        </div>
+    )
+}
+export default App
+
+// install VSCode Extension   'ES7 React/Redux/GraphQL/React-Native snippets'
+//  you can use 'rafce'
+
+
+
